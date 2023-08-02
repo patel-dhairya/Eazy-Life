32ccc9d7fa3bd5264b5cf930d8bfaf2ae67f9461
Dhairya Patel <p5d7@proton.me>
2023-08-02 22:24:41 +0530

    Downgrade of Java version to 17 from 20
    >> Summary
    > This commit downgrade project version of Java from 20 to 17 alongside JavaFX

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
-module eazyLife {
+module com.personal.easylife {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
     requires java.desktop;
     requires jdk.jsobject;
 
-    opens com.jdojo.intro to javafx.graphics, javafx.base;
+
+    opens com.personal.easylife to javafx.fxml;
+    exports com.personal.easylife;
 }
